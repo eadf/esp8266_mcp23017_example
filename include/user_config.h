a6c60ef74acf0402b52adc9bd1ae22e644f373c1
EAD Fritz <ead.fritz@gmail.com>
2015-03-13 19:37:40 +0100

    configured the demo to use the same pin numbering as the "i2c-driver" version used

--- include/user_config.h
+++ include/user_config.h
@@ -1,8 +1,8 @@
 #ifndef _USER_CONFIG_H_
 #define _USER_CONFIG_H_
 
-#define I2C_MASTER_SDA_GPIO 2
-#define I2C_MASTER_SCL_GPIO 0
+#define I2C_MASTER_SDA_GPIO 0
+#define I2C_MASTER_SCL_GPIO 2
 #define I2C_MASTER_HALF_CYCLE 3
 
 #endif
